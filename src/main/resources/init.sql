e754521b40f67d1852e2eeb7c63040abd483d6cb
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-01-30 15:35:53 +0300

    Add sql and docker-compose files

---
+++ src/main/resources/init.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS stepik;
+
+CREATE DATABASE IF NOT EXISTS stepik;
+
+USE stepik;
+
+DROP TABLE IF EXISTS book;
+
+CREATE TABLE book
+(
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title   VARCHAR(50),
+    author  VARCHAR(30),
+    price   DECIMAL(8, 2),
+    amount  INT
+);
+
+INSERT INTO book(title, author, price, amount)
+VALUES ('Мастер и Маргарита', 'Булгаков М.А.', 670.99, 3),
+       ('Белая гвардия>', 'Булгаков М.А.', 540.50, 5),
+       ('Идиот', 'Достоевский Ф.М.', 460.00, 10),
+       ('Братья Карамазовы', 'Достоевский Ф.М.', 799.01, 2),
+       ('Стихотворения и поэмы', 'Есенин С.А.', 650.00, 15);
+
+SELECT * FROM book;
