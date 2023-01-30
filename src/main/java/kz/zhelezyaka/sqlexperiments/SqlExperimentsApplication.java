fa64e0eaaec3366ce7aeeb1e1e2e1dee2c8fb342
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-01-30 15:33:02 +0300

    Init project

---
+++ src/main/java/kz/zhelezyaka/sqlexperiments/SqlExperimentsApplication.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.sqlexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlExperimentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlExperimentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/zhelezyaka/sqlexperiments/SqlExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.sqlexperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlExperimentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
