59e16db5ac25d2030a666cfc0687745cf5006acd
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-02-26 18:48:59 +0300

    Data sampling with calculation, group functions

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -31,6 +31,3 @@ VALUES ('Мастер и Маргарита', 'Булгаков М.А.', 670.99,
 SELECT *
 FROM book;
 
-SELECT author, MIN(price) AS 'Минимальная_цена', MAX(price) AS 'Максимальная_цена', AVG(price) AS 'Средняя_цена'
-FROM book
-GROUP BY author;
--- src/main/resources/select.sql
+++ src/main/resources/select.sql
@@ -34,3 +34,10 @@ GROUP BY author;
 SELECT author, MIN(price) AS 'Минимальная_цена', MAX(price) AS 'Максимальная_цена', AVG(price) AS 'Средняя_цена'
 FROM book
 GROUP BY author;
+
+SELECT author,
+       ROUND(SUM(price * book.amount), 2)                       AS 'Стоимость',
+       ROUND(SUM((price * book.amount) * 0.18 / (1 + 0.18)), 2) AS 'НДС',
+       ROUND(SUM((price * book.amount) / (1 + 0.18)), 2)        AS 'Стоимость_без_НДС'
+FROM book
+GROUP BY author;
