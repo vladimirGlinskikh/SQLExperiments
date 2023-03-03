9e82ffd0079b33a376954473d5803b554e4cae04
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-03-03 21:10:01 +0300

    Using a nested query in an expression

--- src/main/resources/select.sql
+++ src/main/resources/select.sql
@@ -67,3 +67,8 @@ FROM book
 WHERE price <= (SELECT AVG(price)
                 FROM book)
 ORDER BY price DESC;
+
+SELECT author, title, price
+FROM book
+WHERE price <= (SELECT MIN(price) + 150 FROM book)
+ORDER BY price ASC;
