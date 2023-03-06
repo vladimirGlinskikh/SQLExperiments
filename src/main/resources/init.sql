bc19431bb1f6ba6c884f6061a44c538e0d4dafd1
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-03-06 12:33:44 +0300

    Requests for deletion

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -47,4 +47,3 @@ FROM supply;
 
 
 
-
--- src/main/resources/select.sql
+++ src/main/resources/select.sql
@@ -135,3 +135,11 @@ SET book.amount = book.amount + supply.amount,
     book.price  = ((book.price + supply.price) / 2)
 WHERE book.title = supply.title
   AND book.author = supply.author;
+
+DELETE
+FROM supply
+WHERE author IN
+      (SELECT author
+       FROM book
+       GROUP BY author
+       HAVING SUM(amount) >= 10);
