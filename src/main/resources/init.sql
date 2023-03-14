f2acb8db8ef69348a24a314daa5943ea4227dc95
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-03-14 16:32:40 +0300

    Task is done

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -7,6 +7,7 @@ USE stepik;
 DROP TABLE IF EXISTS book;
 DROP TABLE IF EXISTS supply;
 DROP TABLE IF EXISTS trip;
+DROP TABLE IF EXISTS fine;
 
 CREATE TABLE book
 (
@@ -36,6 +37,17 @@ CREATE TABLE trip
     date_last  DATE
 );
 
+CREATE TABLE fine
+(
+    fine_id        INT PRIMARY KEY AUTO_INCREMENT,
+    `name`         VARCHAR(30),
+    number_plate   VARCHAR(6),
+    violation      VARCHAR(50),
+    sum_fine       DECIMAL(8, 2),
+    date_violation DATE,
+    date_payment   DATE
+);
+
 INSERT INTO book(title, author, price, amount)
 VALUES ('Мастер и Маргарита', 'Булгаков М.А.', 670.99, 3),
        ('Белая гвардия', 'Булгаков М.А.', 540.50, 5),
@@ -49,7 +61,7 @@ VALUES ('Лирика', 'Пастернак Б.Л.', 518.99, 2),
        ('Белая гвардия', 'Булгаков М.А.', 540.50, 7),
        ('Идиот', 'Достоевский Ф.М.', 360.80, 3);
 
-INSERT INTO trip(trip.name, city, per_diem, date_first, date_last)
+INSERT INTO trip(`name`, city, per_diem, date_first, date_last)
 VALUES ('Баранов П.Е.', 'Москва', 700, '2020-01-12', '2020-01-17'),
        ('Абрамова К.А.', 'Владивосток', 450, '2020-01-14', '2020-01-27'),
        ('Семенов И.В.', 'Москва', 700, '2020-01-23', '2020-01-31'),
@@ -71,12 +83,25 @@ VALUES ('Баранов П.Е.', 'Москва', 700, '2020-01-12', '2020-01-17'
        ('Абрамова К.А.', 'Владивосток', 450, '2020-07-02', '2020-07-13'),
        ('Баранов П.Е.', 'Воронеж', 450, '2020-07-19', '2020-07-25');
 
+INSERT INTO fine(`name`, number_plate, violation, sum_fine, date_violation, date_payment)
+VALUES ('Баранов П.Е.', 'P523BT', 'Превышение скорости(от 40 до 60)', 500.00, '2020-01-12', '2020-01-17'),
+       ('Абрамова К.А.', 'О111АВ', 'Проезд на запрещающий сигнал', 1000.00, '2020-01-14', '2020-02-27'),
+       ('Яковлев Г.Р.', 'Т330ТТ', 'Превышение скорости(от 20 до 40)', 500.00, '2020-01-23', '2020-02-23'),
+       ('Яковлев Г.Р.', 'М701АА', 'Превышение скорости(от 20 до 40)', NULL, '2020-01-12', NULL),
+       ('Колесов С.П.', 'К892АХ', 'Превышение скорости(от 20 до 40)', NULL, '2020-02-01', NULL),
+       ('Баранов П.Е.', 'Р523ВТ', 'Превышение скорости(от 40 до 60)', NULL, '2020-02-14', NULL),
+       ('Абрамова К.А.', 'О111АВ', 'Проезд на запрещающий сигнал', NULL, '2020-02-23', NULL),
+       ('Яковлев Г.Р.', 'Т330ТТ', 'Проезд на запрещающий сигнал', NULL, '2020-03-03', NULL);
+
 
 SELECT *
 FROM book;
 
 SELECT *
 FROM trip;
+
+SELECT *
+FROM fine;
 
 
 
